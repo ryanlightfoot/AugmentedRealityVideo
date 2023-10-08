993a9b6233108c2d88f43ef8bc2b660cbba1a7e3
ryanlight <ryanlightfoot509@gmail.com>
2023-10-08 14:38:49 +0200

    first commit

--- main.py
+++ main.py
@@ -13,12 +13,15 @@ kp1, des1 = sift.detectAndCompute(imgTarget, None)
 # Initialize the camera
 cap = cv2.VideoCapture(0)  # 0 represents the default camera (you can change it if you have multiple cameras)
 
+# Load the video
+video = cv2.VideoCapture('video.mp4')  # Replace 'video.mp4' with the path to your video file.
+
 while True:
     # Read a frame from the camera
     ret, frame = cap.read()
     if not ret:
         break
-    
+
     # Convert the frame to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -40,9 +43,6 @@ while True:
         if m.distance < 0.75 * n.distance:
             good_matches.append(m)
 
-    # Draw the matches
-    imgMatches = cv2.drawMatches(imgTarget, kp1, gray, kp2, good_matches, None)
-
     # Extract the location of the matches
     if len(good_matches) > 4:
         src_pts = np.float32([kp1[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
@@ -51,23 +51,35 @@ while True:
         # Calculate the homography matrix
         M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
 
-        # Get the corners of the target image
-        h, w = imgTarget.shape
-        corners = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+        if M is not None:
+            # Get the dimensions of the target image
+            h, w = imgTarget.shape[:2]
 
-        # Transform the corners using the homography matrix
-        transformed_corners = cv2.perspectiveTransform(corners, M)
+            # Transform the corners using the homography matrix
+            corners = np.float32([[0, 0], [0, h], [w, h], [w, 0]]).reshape(-1, 1, 2)
+            transformed_corners = cv2.perspectiveTransform(corners, M)
 
-        # Draw a square around the target image
-        frame = cv2.polylines(frame, [np.int32(transformed_corners)], True, (0, 255, 0), 3)
+            # Draw a square around the target image
+            frame = cv2.polylines(frame, [np.int32(transformed_corners)], True, (0, 255, 0), 3)
+
+            # Check if the video is open and play it on the identified region
+            if video.isOpened():
+                ret, video_frame = video.read()
+                if ret:
+                    # Warp the video frame onto the target image
+                    warped_video_frame = cv2.warpPerspective(video_frame, M, (frame.shape[1], frame.shape[0]))
+                    
+                    # Overlay the video frame on the target image
+                    frame = cv2.addWeighted(frame, 1, warped_video_frame, 0.7, 0)
 
     # Display the result
-    cv2.imshow('Target Detection', frame)
+    cv2.imshow('AR Video', frame)
 
     # Exit the loop if the 'q' key is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the camera and close all windows
+# Release the camera, video, and close all windows
 cap.release()
+video.release()
 cv2.destroyAllWindows()
